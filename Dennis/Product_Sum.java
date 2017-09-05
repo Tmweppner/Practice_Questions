3a0c8ed95e77b5e3e92d93d336e7d06f56c27697
tmweppner <tmweppne@buffalo.edu>
2017-09-04 22:30:18 -0500

    added first question

---
+++ Dennis/Product_Sum.java
@@ -0,0 +1,23 @@
+/*
+Given two integers a and b. Write a program to find the number of digits in the product of these two integers.
+
+Input:
+First line contains an integer T denoting number of test cases. The next T lines contains 2 space separated integers a and b each.
+
+
+Output:
+For each test case print the number of digits in the product of the two numbers.
+
+Constraints:
+1<=T<=10^5
+-10^5<=a,b<=10^5
+
+Example:
+Input: 
+1 
+12 4 
+-24 33 
+Output: 
+2 
+3
+*/
