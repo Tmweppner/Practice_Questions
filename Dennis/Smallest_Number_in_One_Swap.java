9c35bbb1855f2a2788fce3dfa17844393de0043e
tmweppner <tmweppne@buffalo.edu>
2017-09-06 00:07:32 -0500

    adding second question

---
+++ Dennis/Smallest_Number_in_One_Swap.java
@@ -0,0 +1,27 @@
+/*
+Given a non-negative number N. The task is to apply at most one swap operation on the number N so that the resultant is the smallest possible number.
+
+Note: Input and Output should not contain leading zeros.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains a number N as input.
+
+Output:
+For each test case, print the smallest number possible. if it is not possible to make smallest number than N then print N as it is.
+
+Constraints:
+1<=T<=300
+2<=|Number length |<=105
+
+Example:
+Input:
+2
+9625635
+3268
+
+Output:
+2695635
+2368
+*/
+
+
